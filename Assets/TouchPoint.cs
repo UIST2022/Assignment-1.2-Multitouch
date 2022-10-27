7e14d0696390a6f8979a679b43c05662d27ea117
rudygarrido <rudy.garrido.veliz@gmail.com>
2022-10-27 12:43:56 +0200

    Base Code

---
+++ Assets/TouchPoint.cs
@@ -0,0 +1,28 @@
+﻿using TUIO;
+using UnityEngine;
+
+namespace Assets
+{
+    [System.Serializable]
+    public class TouchPoint
+    {
+
+        public TouchPoint(TuioCursor tuioCursor)
+        {
+            X = tuioCursor.getX();
+            Y = tuioCursor.getY();
+            Active = tuioCursor.getTuioState() != TuioContainer.TUIO_REMOVED;
+        }
+
+        public float X { get; set; }
+        public float Y { get; set; }
+        public bool Active { get; set; } 
+        public Vector3 fromTUIO()
+        {
+            return Camera.main.ViewportToWorldPoint(new Vector3(X, 1 - Y, -Camera.main.transform.position.z));
+        }
+    }
+}
+
+
+
---
+++ Assets/TouchReceiver.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+using TUIO;
+using System.Collections.Generic;
+using Assets;
+
+public partial class TouchReceiver : MonoBehaviour
+{
+
+    [SerializeField]
+    Mesh debugMesh;
+    [SerializeField]
+    Material startMat;
+    [SerializeField]
+    Material currentMat;
+    [SerializeField]
+    GestureState currentState = GestureState.None;
+    public TouchPoint FirstTouchPoint { get; set; }
+    public TouchPoint SecondTouchPoint { get; set; }
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        DrawDebugMeshes();
+        HandleTouchInput();
+    }
+
+    void HandleTouchInput()
+    {
+        //TODO
+        //Simple example:
+        if (FirstTouchPoint != null)
+        {
+            this.transform.position = FirstTouchPoint.fromTUIO();
+        }
+    }
+
+    /// <summary>
+    /// Processes Tuio-Events
+    /// </summary>
+    /// <param name="events"></param>
+	void processEvents(ArrayList events)
+    {
+        foreach (BBCursorEvent cursorEvent in events)
+        {
+            TuioCursor myCursor = cursorEvent.cursor;
+            if (myCursor.getCursorID() == 0)
+            {
+                FirstTouchPoint = new TouchPoint(myCursor);
+            }
+            if (myCursor.getCursorID() == 1)
+            {
+                SecondTouchPoint = new TouchPoint(myCursor);
+            }
+        }
+    }
+
+    void DrawDebugMeshes()
+    {
+        DrawDebugMeshForTouchPoint(FirstTouchPoint);
+        DrawDebugMeshForTouchPoint(SecondTouchPoint);
+
+    }
+
+    private void DrawDebugMeshForTouchPoint(TouchPoint touchPoint)
+    {
+        if (touchPoint != null && touchPoint.Active)
+        {
+            DrawDebugMeshFor(touchPoint.fromTUIO());
+        }
+    }
+
+    private void DrawDebugMeshFor(Vector3 touchPointVector)
+    {
+        Graphics.DrawMesh(debugMesh,
+                            Matrix4x4.TRS(touchPointVector, Quaternion.identity,
+                                new Vector3(0.05f, 0.05f, 0.05f)), currentMat, 0);
+    }
+}
